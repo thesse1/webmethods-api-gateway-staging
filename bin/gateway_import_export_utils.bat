7c26de8dae080d0b9f2aa6b9c18f8795b80afe78
Dr. Thomas Hesse <thomas.hesse@softwareag.com>
2021-08-24 14:42:00 +0200

    Intermediate commit

--- bin/gateway_import_export_utils.bat
+++ bin/gateway_import_export_utils.bat
@@ -9,27 +9,31 @@ SHIFT
 
 :loop
 IF NOT "%1"=="" (
-    IF "%1"=="--api_name" (
-        SET PROJECT_NAME=%2
-        SHIFT
-    ) ELSE IF "%1"=="--environment" (
-        SET ENVIRONMENT=%2
-        SHIFT
-    )
-    IF "%1"=="--apigateway_url" (
-        SET GATEWAY_URL=%2
-        SHIFT
-    )
+  IF "%1"=="--tenant" (
+      SET TENANT=%2
+      SHIFT
+  )
+  IF "%1"=="--api_name" (
+      SET PROJECT_NAME=%2
+      SHIFT
+  ) ELSE IF "%1"=="--environment" (
+      SET ENVIRONMENT=%2
+      SHIFT
+  )
+  IF "%1"=="--apigateway_url" (
+      SET GATEWAY_URL=%2
+      SHIFT
+  )
 	IF "%1"=="--apigateway_username" (
-        SET GATEWAY_USERNAME=%2
-        SHIFT
-    )
+      SET GATEWAY_USERNAME=%2
+      SHIFT
+  )
 	IF "%1"=="--apigateway_password" (
-        SET GATEWAY_PASSWORD=%2
-        SHIFT
-    )
-    SHIFT
-    GOTO :loop
+      SET GATEWAY_PASSWORD=%2
+      SHIFT
+  )
+  SHIFT
+  GOTO :loop
 )
 
 IF "%ACTION%" == "--importapi" (
@@ -46,53 +50,53 @@ IF "%ACTION%" == "--importapi" (
 )
 
 :importapi
-IF EXIST %CURRENT_DIR%\apis\%PROJECT_NAME%\assets (
- powershell Compress-Archive -Path %CURRENT_DIR%\apis\%PROJECT_NAME%\assets\* -DestinationPath %CURRENT_DIR%\%PROJECT_NAME%.zip -Force
+IF EXIST %CURRENT_DIR%\%TENANT%\apis\%PROJECT_NAME%\assets (
+ powershell Compress-Archive -Path %CURRENT_DIR%\%TENANT%\apis\%PROJECT_NAME%\assets\* -DestinationPath %CURRENT_DIR%\%PROJECT_NAME%.zip -Force
  curl -k -i -X POST %GATEWAY_URL%/rest/apigateway/archive?overwrite=*,aliases -H "Content-Type: application/zip" -H "Accept:application/json" --data-binary @"%CURRENT_DIR%\%PROJECT_NAME%.zip" --user %GATEWAY_USERNAME%:%GATEWAY_PASSWORD%
  del "%CURRENT_DIR%\%PROJECT_NAME%.zip"
  goto :EOF
 ) ELSE (
-  echo "Folder %CURRENT_DIR%\apis\%PROJECT_NAME%\assets does not exist"
+  echo "Folder %CURRENT_DIR%\%TENANT%\apis\%PROJECT_NAME%\assets does not exist"
   echo "API with name %PROJECT_NAME% does not exist"
   goto :EOF
 )
 
 :exportapi
-IF EXIST %CURRENT_DIR%\apis\%PROJECT_NAME%\assets (
-  rmdir "%CURRENT_DIR%\apis\%PROJECT_NAME%\assets" /s /q
-  mkdir "%CURRENT_DIR%\apis\%PROJECT_NAME%\assets"
-  curl %GATEWAY_URL%/rest/apigateway/archive -k -d @"%CURRENT_DIR%\apis\%PROJECT_NAME%\export_payload.json" --output %CURRENT_DIR%\%PROJECT_NAME%.zip --user %GATEWAY_USERNAME%:%GATEWAY_PASSWORD% -H "x-HTTP-Method-Override: GET" -H "Content-Type:application/json"
-  powershell Expand-Archive -Path %CURRENT_DIR%\%PROJECT_NAME%.zip -DestinationPath %CURRENT_DIR%\apis\%PROJECT_NAME%\assets -Force
+IF EXIST %CURRENT_DIR%\%TENANT%\apis\%PROJECT_NAME%\assets (
+  rmdir "%CURRENT_DIR%\%TENANT%\apis\%PROJECT_NAME%\assets" /s /q
+  mkdir "%CURRENT_DIR%\%TENANT%\apis\%PROJECT_NAME%\assets"
+  curl %GATEWAY_URL%/rest/apigateway/archive -k -d @"%CURRENT_DIR%\%TENANT%\apis\%PROJECT_NAME%\export_payload.json" --output %CURRENT_DIR%\%PROJECT_NAME%.zip --user %GATEWAY_USERNAME%:%GATEWAY_PASSWORD% -H "x-HTTP-Method-Override: GET" -H "Content-Type:application/json"
+  powershell Expand-Archive -Path %CURRENT_DIR%\%PROJECT_NAME%.zip -DestinationPath %CURRENT_DIR%\%TENANT%\apis\%PROJECT_NAME%\assets -Force
   del "%CURRENT_DIR%\%PROJECT_NAME%.zip"
   goto :EOF
 ) ELSE (
-  echo "Folder %CURRENT_DIR%\apis\%PROJECT_NAME%\assets does not exist"
+  echo "Folder %CURRENT_DIR%\%TENANT%\apis\%PROJECT_NAME%\assets does not exist"
   echo "API with name %PROJECT_NAME% does not exist"
   goto :EOF
 )
 
 :importconfig
-IF EXIST %CURRENT_DIR%\configuration\%ENVIRONMENT%\assets (
- powershell Compress-Archive -Path %CURRENT_DIR%\configuration\%ENVIRONMENT%\assets\* -DestinationPath %CURRENT_DIR%\%ENVIRONMENT%.zip -Force
+IF EXIST %CURRENT_DIR%\%TENANT%\configuration\%ENVIRONMENT%\assets (
+ powershell Compress-Archive -Path %CURRENT_DIR%\%TENANT%\configuration\%ENVIRONMENT%\assets\* -DestinationPath %CURRENT_DIR%\%ENVIRONMENT%.zip -Force
  curl -k -i -X POST %GATEWAY_URL%/rest/apigateway/archive?overwrite=* -H "Content-Type: application/zip" -H "Accept:application/json" --data-binary @"%CURRENT_DIR%\%ENVIRONMENT%.zip" --user %GATEWAY_USERNAME%:%GATEWAY_PASSWORD%
  del "%CURRENT_DIR%\%ENVIRONMENT%.zip"
  goto :EOF
 ) ELSE (
-  echo "Folder %CURRENT_DIR%\configuration\%ENVIRONMENT%\assets does not exist"
+  echo "Folder %CURRENT_DIR%\%TENANT%\configuration\%ENVIRONMENT%\assets does not exist"
   echo "Environment with name %ENVIRONMENT% does not exist"
   goto :EOF
 )
 
 :exportconfig
-IF EXIST %CURRENT_DIR%\configuration\%ENVIRONMENT%\assets (
-  rmdir "%CURRENT_DIR%\configuration\%ENVIRONMENT%\assets" /s /q
-  mkdir "%CURRENT_DIR%\configuration\%ENVIRONMENT%\assets"
-  curl %GATEWAY_URL%/rest/apigateway/archive -k -d @"%CURRENT_DIR%\configuration\%ENVIRONMENT%\export_payload.json" --output %CURRENT_DIR%\%ENVIRONMENT%.zip --user %GATEWAY_USERNAME%:%GATEWAY_PASSWORD% -H "x-HTTP-Method-Override: GET" -H "Content-Type:application/json"
-  powershell Expand-Archive -Path %CURRENT_DIR%\%ENVIRONMENT%.zip -DestinationPath %CURRENT_DIR%\configuration\%ENVIRONMENT%\assets -Force
+IF EXIST %CURRENT_DIR%\%TENANT%\configuration\%ENVIRONMENT%\assets (
+  rmdir "%CURRENT_DIR%\%TENANT%\configuration\%ENVIRONMENT%\assets" /s /q
+  mkdir "%CURRENT_DIR%\%TENANT%\configuration\%ENVIRONMENT%\assets"
+  curl %GATEWAY_URL%/rest/apigateway/archive -k -d @"%CURRENT_DIR%\%TENANT%\configuration\%ENVIRONMENT%\export_payload.json" --output %CURRENT_DIR%\%ENVIRONMENT%.zip --user %GATEWAY_USERNAME%:%GATEWAY_PASSWORD% -H "x-HTTP-Method-Override: GET" -H "Content-Type:application/json"
+  powershell Expand-Archive -Path %CURRENT_DIR%\%ENVIRONMENT%.zip -DestinationPath %CURRENT_DIR%\%TENANT%\configuration\%ENVIRONMENT%\assets -Force
   del "%CURRENT_DIR%\%ENVIRONMENT%.zip"
 goto :EOF
 ) ELSE (
-  echo "Folder %CURRENT_DIR%\configuration\%ENVIRONMENT%\assets does not exist"
+  echo "Folder %CURRENT_DIR%\%TENANT%\configuration\%ENVIRONMENT%\assets does not exist"
   echo "Environment with name %ENVIRONMENT% does not exist"
   goto :EOF
 )
